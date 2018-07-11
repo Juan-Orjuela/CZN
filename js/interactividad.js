d77134d4a2ff0dcb2c96062682e93146f9fd6862
Juan Orjuela <orjuelajuans@gmail.com>
2018-07-11 14:52:30 -0500

    Antes de animaciones

--- js/interactividad.js
+++ js/interactividad.js
@@ -1,48 +1,62 @@
-$(document).ready(function(){
-    var btnHome = $('.nav-home a'),
-        navHome = $('.nav-home'),
-        velo = $('.velo');
+$(document).ready(function () {
+  var btnHome = $('.nav-home a'),
+    navHome = $('.nav-home'),
+    velo = $('.velo');
 
-        navHome.on({
-            mouseenter: function() {
-              velo.addClass( "oculto" );
-            }, mouseleave: function() {
-              velo.removeClass( "oculto" );
-            }
-          });
+  navHome.on({
+    mouseenter: function () {
+      velo.addClass("oculto");
+    }, mouseleave: function () {
+      velo.removeClass("oculto");
+    }
+  });
 
-    //Animar letras
-    var lineDrawing = anime({
-      targets: '.letra path',
-      strokeDashoffset: [anime.setDashoffset, 0],
-      easing: 'easeInOutSine',
-      duration: 4500,
-      delay: function(el, i) { return i * 250 },
-      direction: 'alternate',
-      loop: false
-    });
+  //Animar letras
+  var lineDrawing = anime({
+    targets: '.letra path',
+    strokeDashoffset: [anime.setDashoffset, 0],
+    easing: 'easeInOutSine',
+    duration: 4500,
+    delay: function (el, i) { return i * 250 },
+    direction: 'alternate',
+    loop: false
+  });
 
-    //Vistas
-    $('.vista-btn').on('click', function() {
-      var vista = $(this).data('vista');
-     
-      console.log(vista);
-      $('#fullwrap').attr('data-mirar', vista);
-    });
+  //Vistas
+  $('#lugar_04 .vista-btn').on('click', function () {
+    var vista = $(this).data('vista');
+    $('#fullwrap').attr('data-mirar', vista);
+  });
 
-    //Slider Area
-    $("#flat-slider")
+  //Slider Area
+  $("#area-slider")
     .slider({
-        max: 85,
-        min: 45,
-        range: true,
-        values: [50, 80],
-        step: 2.5
+      max: 85,
+      min: 45,
+      range: true,
+      values: [50, 80],
+      step: 2.5
     })
     .slider("pips", {
-        first: "pip",
-        last: "pip"
-    }).on("slidechange", function(e,ui) {
-      $("#metros").html( "Entre " + ui.values[0] +  " y " + ui.values[1]+ " MT<span>2</span>");
-  });
+      first: "pip",
+      last: "pip"
+    }).on("slidechange", function (e, ui) {
+      $("#metros").html("Entre " + ui.values[0] + " y " + ui.values[1] + " MT<span>2</span>");
+    });
+
+    //Slider pisos
+    $("#pisos-slider")
+    .slider({
+      max: 25,
+      min: 10,
+      range: true,
+      values: [10, 25],
+      step: 1
+    })
+    .slider("pips", {
+      first: "pip",
+      last: "pip"
+    }).on("slidechange", function (e, ui) {
+      $("#pisos").html("Entre " + ui.values[0] + " y " + ui.values[1]);
+    });
 });
